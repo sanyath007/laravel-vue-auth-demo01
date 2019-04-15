a14634c08dc14778247e6402687583af47bac188
sanyath007 <sanyath007@gmail.com>
2019-04-15 10:01:39 +0700

    Change default auth guard to api and token to jwt.

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,8 @@ Route::post('auth/register', 'AuthController@register');
 Route::post('auth/login', 'AuthController@login');
 Route::group(['middleware' => 'jwt.auth'], function() {
     Route::get('auth/user', 'AuthController@user');
+    Route::post('auth/logout', 'AuthController@logout');
 });
 Route::group(['middleware' => 'jwt.refresh'], function() {
     Route::get('auth/refresh', 'AuthController@refresh');
-});
-Route::group(['middleware' => 'jwt.auth'], function() {
-    Route::get('auth/logout', 'AuthController@logout');
 });
